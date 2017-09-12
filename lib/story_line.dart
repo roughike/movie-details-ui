776684bbb272ec3c88766ae39dedd42593c8be75
Iiro Krankka <iiro.krankka@gmail.com>
2017-09-12 20:33:52 +0300

    Fixed the "more" button in the storyline widget to just display under the text.

--- lib/story_line.dart
+++ lib/story_line.dart
@@ -11,46 +11,38 @@ class Storyline extends StatelessWidget {
     var theme = Theme.of(context);
     var textTheme = Theme.of(context).textTheme;
 
-    return new Stack(
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            new Text(
-              'Story line',
-              style: textTheme.subhead.copyWith(fontSize: 18.0),
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: new Text(
-                storyline,
-                style: textTheme.body1
-                    .copyWith(color: Colors.black45, fontSize: 16.0),
-              ),
-            ),
-          ],
+        new Text(
+          'Story line',
+          style: textTheme.subhead.copyWith(fontSize: 18.0),
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            storyline,
+            style:
+                textTheme.body1.copyWith(color: Colors.black45, fontSize: 16.0),
+          ),
         ),
         // No expand-collapse in this tutorial, we just slap the "more"
-        // button over the text like in the mockup.
-        new Positioned(
-          bottom: 0.0,
-          right: 0.0,
-          child: new Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              new Text(
-                'more',
-                style: textTheme.body1
-                    .copyWith(fontSize: 16.0, color: theme.accentColor),
-              ),
-              new Icon(
-                Icons.keyboard_arrow_down,
-                size: 18.0,
-                color: theme.accentColor,
-              ),
-            ],
-          ),
+        // button below the text like in the mockup.
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            new Text(
+              'more',
+              style: textTheme.body1
+                  .copyWith(fontSize: 16.0, color: theme.accentColor),
+            ),
+            new Icon(
+              Icons.keyboard_arrow_down,
+              size: 18.0,
+              color: theme.accentColor,
+            ),
+          ],
         ),
       ],
     );
